c09ebf5272d26e3f0f0498b5b3420325fb63a065
Gitesh502 <raju.aag@gmail.com>
2016-12-31 23:49:25 +0530

    Dec 31 two

---
+++ KnowCostWeb/app/Controllers/LoginController.js
@@ -0,0 +1,37 @@
+﻿var LoginController = function ($scope, LoginFactory, $window, toaster,$controller) {
+    $controller('CommonController', { $scope: $scope, $window: $window })
+    $scope.UserLoginDetails = {
+        Email: "",
+        Password: "",
+        RememberMe: false,
+        returnUrl:""
+    }
+
+    $scope.SignIn = function () {
+        var result = LoginFactory($scope.UserLoginDetails);
+        result.then(function (result) {
+            console.log(result)
+            if (result.success) {
+                $window.location.href = '/Chat/Home/Index' ;
+
+            } else {
+                switch (result.Id) {
+                    case 2:
+                        toaster.pop('danger', "Error", "Your account is lockedout please contact administrator");
+                        break;
+                    case 3:
+                        toaster.pop('danger', "Error", "Your account requires TwoFactorAuthentication please contact administrator");
+                        break;
+                    case 0:
+                        toaster.pop({type: 'error',title:  "Error",body: "Invalid username or password"});
+                        break;
+                    default:
+                        toaster.pop('danger', "Error", "Invalid login attempt.");
+                        break;
+
+                }
+            }
+        })
+    }
+}
+LoginController.$inject = ['$scope', 'LoginFactory', '$window', 'toaster', '$controller'];
